473149221656515fe91d78152c50dca3b20ccdeb
Anton Losman <anton.losman@smit.ee>
2023-01-17 12:41:41 +0200

    [add] - FizzBuzz MainApp class added

---
+++ src/main/java/org/example/tdd/MainApp.java
@@ -0,0 +1,13 @@
+package org.example.tdd;
+
+public class MainApp {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 100; i++) {
+
+      System.out.println(FizzBuzz.compute(i));
+    }
+
+  }
+}
